dab36f1985e051ec5ec5e9bf8808aa74127c0167
RohaanMooken <rohaanmooken@gmail.com>
2024-02-24 04:11:01 -0500

    Module 4 Application Dynamic Pages (React.js) Completed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -102,15 +102,6 @@ def get_dealerships(request, state="All"):
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request,dealer_id):
     if dealer_id:
-        endpoint = "/fetchDealer/" + str(dealer_id)
-        dealership = get_request(endpoint)
-        return JsonResponse({ "status":200, "dealer": dealership })
-    else:
-        return JsonResponse({ "status":400, "message", "Bad Request" })
-
-# Create a `get_dealer_details` view to render the dealer details
-def get_dealer_details(request, dealer_id):
-    if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -119,7 +110,17 @@ def get_dealer_details(request, dealer_id):
             review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({ "status":400, "message", "Bad Request" })
+        return JsonResponse({ "status":400, "message": "Bad Request" })
+    
+
+# Create a `get_dealer_details` view to render the dealer details
+def get_dealer_details(request, dealer_id):
+    if dealer_id:
+        endpoint = "/fetchDealer/" + str(dealer_id)
+        dealership = get_request(endpoint)
+        return JsonResponse({ "status":200, "dealer": dealership })
+    else:
+        return JsonResponse({ "status":400, "message": "Bad Request" })
 
 # Create a `add_review` view to submit a review
 def add_review(request):
